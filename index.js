61678795115a7067889036dbe122a6941a5581fa
Alkosik <alkos.yt@gmail.com>
2021-01-17 22:52:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -128,16 +128,21 @@ client.on('message', message => {
 
         })
     } else if (!message.author.bot) {
+
         const args = message.content.slice(prefix.length).trim().split(/ +/);
-        const command = args.shift().toLowerCase();
-        if (!client.commands.has(command)) return;
+        const commandName = args.shift().toLowerCase();
+
+        if (!client.commands.has(commandName)) return;
+
+        const command = client.commands.get(commandName);
 
         try {
-            client.commands.get(command).execute(message, args);
+            command.execute(message, args);
         } catch (error) {
             console.error(error);
             message.reply('Egzekucja komendy zakonczyla sie niepowodzeniem!');
         }
+        
         message.delete();
     }
 })
