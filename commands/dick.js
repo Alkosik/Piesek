565f230c88b7e9ac1b28ede51ae00e5c0c1b5210
Alkosik <alkos.yt@gmail.com>
2021-01-17 22:37:59 +0100

    Giga update

---
+++ commands/dick.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'dick',
+	description: 'Długość dicka',
+	execute(message, args) {
+		if (message.author.username === 'Alkosik'){
+			return message.reply('alkosik muj pan ma 69cm w dicku')
+		}
+		let dick_lenght = Math.random() * (24 - 0.1) + 0.1;
+		let rounded_dick_lenght = Number(dick_lenght.toFixed(1))
+		message.channel.send(`<@${message.author.id}>, Twój dick ma długość: ` + rounded_dick_lenght + 'cm.');
+	},
+};
--- index.js
+++ index.js
@@ -1,24 +1,43 @@
+const fs = require('fs');
 const Discord = require('discord.js');
+const {
+    prefix
+} = require('./config.json');
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 //const config = require("./")
 const token = process.env.TOKEN;
 const http = require("http");
 const utf8 = require('utf8');
 const mysql = require('mysql');
-const { connect } = require('http2');
+const {
+    connect
+} = require('http2');
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: "www5056_gsmaindb"
-  });
+});
 
-let levelup = 15; // Level Up EXP
+let levelup = 100; // Level Up EXP
 let purple = `RANDOM`;
 
 function generateXp() { //Generating EXP
-    return Math.floor(Math.random() * (10 - 5 + 1))+ 5; // Amount of EXP
+    return Math.floor(Math.random() * (10 - 5 + 1)) + 5; // Amount of EXP
+}
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
 }
 
 client.once('ready', () => {
@@ -54,47 +73,71 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 //client.user.setPresence({ game: { name: 'Gang Słoni', type: "streaming", url: "https://gangsloni.pl"}});
 
 http
-  .createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write(utf8.encode("Gang Słoni Dev Team"));
-    response.end();
-  })
-  .listen(process.env.PORT);
+    .createServer((request, response) => {
+        response.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
+        response.write(utf8.encode("Gang Słoni Dev Team"));
+        response.end();
+    })
+    .listen(process.env.PORT);
 console.log("Server listening on port:" + process.env.PORT);
 
 
 client.on('message', message => {
+    if (!message.content.startsWith(prefix) && !message.author.bot) {
+        connection.query(`SELECT * FROM account WHERE id = ${message.author.id}`, function (err, rows) {
+            if (err) throw err;
 
-connection.query(`SELECT * FROM account WHERE id = '${message.author.id}'`, function (err, rows) { //selecting user id
-    if (err) throw err;
+            let sql;
+            let originalXp;
+            let updatedXp;
 
-    connection.pause(rows);
+            if (rows.length < 1) {
+                originalXp = 0;
+                updatedXp = generateXp();
+                sql = `INSERT INTO account (username, id, xp) VALUES ('${message.author.username}', ${message.author.id}, ${updatedXp})`;
+            } else {
+                originalXp = rows[0].xp;
+                updatedXp = originalXp + generateXp();
+                sql = `UPDATE account SET xp = ${updatedXp} WHERE id = '${message.author.id}'`;
 
+                let nxtLvl = rows[0].level * levelup; //how many +1 level per xp points
 
-    //let createTables = `CREATE TABLE IF NOT EXISTS account`;
-    if(rows.length < 1) { //if the user has no info in table | no id it inserts ID - XP - LEVEL 
-        let lvl = 0;
-        sql = `INSERT INTO account (id, xp, level) VALUES ('${message.author.id}', '${generateXp()}', ${lvl})`
-    } else {
-            let xp = rows[0].xp; //xp = the amount xp in database
+                if (nxtLvl <= rows[0].xp) { //If level supass that amount it 
+                    connection.query(`UPDATE account SET level = ${rows[0].level + 1} WHERE id = '${message.author.id}'`) //updates level
+                }
 
-            sql = `UPDATE account SET xp = ${xp + generateXp()} WHERE id = '${message.author.id}'`; //update xp
-
-            let nxtLvl = rows[0].level * levelup;  //how many +1 level per xp points
-
-            if(nxtLvl <= rows[0].xp){ //If level supass that amount it 
-            connection.query(`UPDATE account SET level = ${rows[0].level + 1} WHERE id = '${message.author.id}'`) //updates level
+                //levelup message 
+                if (nxtLvl <= rows[0].xp) {
+                    const lvlup = new Discord.MessageEmbed()
+                        .setDescription(`muj boze, ${message.author.username} wbiles poziom ${rows[0].level + 1}`)
+                        .setColor(purple)
+                    message.channel.send(lvlup)
+                }
             }
 
-            //levelup message 
-            if(nxtLvl <= rows[0].xp){
-            const lvlup = new RichEmbed()
-            .setDescription(`muj boze, ${message.author.username} levelowales na ${rows[0].level + 1}`)
-            .setColor(purple)
-            message.channel.send(lvlup)
+            if (message.author.id != client.user.id) {
+                connection.query(sql);
             }
+
+            if (originalXp < 500 && updatedXp >= 500) {
+                const role = message.member.guild.roles.cache.find(role => role.name === "Elitarne Słonie");
+                message.member.roles.add(role);
+            }
+
+        })
+    } else if (!message.author.bot) {
+        const args = message.content.slice(prefix.length).trim().split(/ +/);
+        const command = args.shift().toLowerCase();
+        if (!client.commands.has(command)) return;
+
+        try {
+            client.commands.get(command).execute(message, args);
+        } catch (error) {
+            console.error(error);
+            message.reply('Egzekucja komendy zakonczyla sie niepowodzeniem!');
+        }
+        message.delete();
     }
-    connection.resume();
-connection.query(sql)
 })
-});
