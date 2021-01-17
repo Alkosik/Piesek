d112d0ea8dc22bf17b4685f76754c5a03566f2b5
Alkosik <alkos.yt@gmail.com>
2021-01-17 19:32:22 +0100

    Leveling System

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+//const config = require("./")
 const token = process.env.TOKEN;
 const http = require("http");
 const utf8 = require('utf8');
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD
+  });
+
+let levelup = 1500; // Level Up EXP
+let purple = `RANDOM`;
+
+function generateXp() { //Generating EXP
+    return Math.floor(Math.random() * (10 - 5 + 1))+ 5; // Amount of EXP
+}
 
 client.once('ready', () => {
     console.log('READY');
@@ -44,4 +58,35 @@ http
     response.end();
   })
   .listen(process.env.PORT);
-console.log("Server listening on port" + process.env.PORT);
+console.log("Server listening on port:" + process.env.PORT);
+
+
+
+
+connection.query(`SELECT * FROM account WHERE id = '${message.author.id}'`, function (err, rows) { //selecting user id
+    if (err) throw err;
+
+    if(rows.length < 1) { //if the user has no info in table | no id it inserts ID - XP - LEVEL 
+        sql = `INSERT INTO account (id, xp, level) VALUES ('${message.author.id}', '${generateXp()}', ${lvl})`
+    } else {
+            let xp = rows[0].xp; //xp = the amount xp in database
+
+            sql = `UPDATE account SET xp = ${xp + generateXp()} WHERE id = '${message.author.id}'`; //update xp
+
+            let nxtLvl = rows[0].level * levelup;  //how many +1 level per xp points
+
+            if(nxtLvl <= rows[0].xp){ //If level supass that amount it 
+            connection.query(`UPDATE account SET level = ${rows[0].level + 1} WHERE id = '${message.author.id}'`) //updates level
+            }
+
+            //levelup message 
+            if(nxtLvl <= rows[0].xp){
+            const lvlup = new RichEmbed()
+            .setDescription(`muj boze, ${message.author.username} levelowales na ${rows[0].level + 1}`)
+            .setColor(purple)
+            message.channel.send(lvlup)
+            }
+    }
+
+connection.query(sql)
+})
