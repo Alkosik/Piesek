b6d0495626dca70bd421e2dc6d109d2751b1de84
Alkosik <alkos.yt@gmail.com>
2021-01-18 09:55:06 +0100

    Age fix

--- commands/wiek.js
+++ commands/wiek.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: 'Twój wiek.',
 	execute(message, args) {
 			let age = Math.random() * (120 - 1) + 1;
-			let rounded_age = Number(iq.toFixed(1))
+			let rounded_age = Number(age.toFixed(1))
 			message.channel.send(`<@${message.author.id}>, Masz ` + rounded_age + ' lat.');
 	},
 };
