3cecb18a6607a42b1abe6b625e6786cdab3cbc19
Alkosik <alkos.yt@gmail.com>
2021-01-18 14:16:03 +0100

    Create wzrost.js

---
+++ commands/wzrost.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'wzrost',
+	description: 'Twój wzrost.',
+	execute(message, args) {
+			let height = Math.random() * (220 - 1) + 1;
+			let rounded_height = Number(height.toFixed(1))
+			message.channel.send(`<@${message.author.id}>, Masz ` + rounded_height + 'cm wzrostu.');
+	},
+};
