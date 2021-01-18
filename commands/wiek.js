30b035a41c4563fc33568ddb99b125ddfbc8818e
Alkosik <alkos.yt@gmail.com>
2021-01-18 09:52:58 +0100

    Age command

---
+++ commands/wiek.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'wiek',
+	description: 'Twój wiek.',
+	execute(message, args) {
+			let age = Math.random() * (120 - 1) + 1;
+			let rounded_age = Number(iq.toFixed(1))
+			message.channel.send(`<@${message.author.id}>, Masz ` + rounded_age + ' lat.');
+	},
+};
