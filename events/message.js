f8a5293583a57b0d522ab43751f29e2663a08855
Alkosik <alkos.yt@gmail.com>
2021-03-08 23:16:24 +0100

    piririiririririiriririririri

--- events/message.js
+++ events/message.js
@@ -3,6 +3,6 @@ module.exports = {
 	execute(message) {
 		// console.log(`${message.author.tag} in #${message.channel.name} sent: ${message.content}`);
         // This functionality will be removed after aprioprate tests are conducted.
-		console.log("rucham kurwy")
+		//console.log("rucham kurwy")
 	},
 };
