da53462d00b8fb560233acba717b0e3dc52364c1
Alkosik <alkos.yt@gmail.com>
2021-04-18 15:07:10 +0200

    essa

---
+++ events/newmember.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: 'guildMemberAdd',
+	execute(member, client) {
+		console.log("kurwa")
+		//client.channels.cache.get('511224486545326100')
+		console.log(`**${member.tag}** dołączył na serwer.`);
+
+		const guild = client.guilds.cache.get("510941195267080214");
+		var memberCount = guild.members.cache.filter(member => !member.user.bot).size;  
+      	var memberCountChannel = client.channels.cache.get("726734001347231784");
+      	memberCountChannel.setName(`Ludzie: ${memberCount} 👤`);
+	},
+};
