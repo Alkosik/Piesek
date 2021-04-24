02f7c53785f54921f0c044fc3575f6862d80c704
Alkosik <alkos.yt@gmail.com>
2021-04-24 13:59:47 +0200

    Update trackStart.js

--- player/trackStart.js
+++ player/trackStart.js
@@ -7,6 +7,12 @@ module.exports = (client, message, track) => {
     .setColor("RED")
     .setTitle(`${client.emotes.music} Teraz leci`)
     .setDescription(`**${track.title}**`)
+    .setThumbnail(track.thumbnail)
+    .addFields(
+		{ name: 'Autor', value: track.author },
+		//{ name: 'Długość', value: track.lengthSeconds + 's', inline: true },
+		//{ name: 'Wyświetlenia', value: track.views, inline: true },
+	)
     //message.channel.send(`${client.emotes.music} - Teraz leci **${track.title}** na kanale ${message.member.voice.channel.name}`);
     message.channel.send(sembed);
 };
