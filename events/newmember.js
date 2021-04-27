87aa72b484f9ba63d9b07126c6fe25e91825b575
Alkosik <alkos.yt@gmail.com>
2021-04-27 22:41:35 +0200

    mem count, rem welcome img

--- events/newmember.js
+++ events/newmember.js
@@ -5,9 +5,11 @@ module.exports = {
 		//client.channels.cache.get('511224486545326100')
 		console.log(`**${member.tag}** dołączył na serwer.`);
 
+		//const guild = client.guilds.cache.find(guild => {guild.id == "510941195267080214"});
 		const guild = client.guilds.cache.get("510941195267080214");
-		var memberCount = guild.members.cache.filter(member => !member.user.bot).size;  
+		//var memberCount = guild.members.cache.filter(member => !member.user.bot).size;  
       	var memberCountChannel = client.channels.cache.get("726734001347231784");
-      	memberCountChannel.setName(`Ludzie: ${memberCount} 👤`);
+		let memCount = guild.memberCount;
+      	memberCountChannel.setName(`Ludzie: ${memCount} 👤`);
 	},
 };
