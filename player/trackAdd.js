8e98ca9480530e567dadb923a2021c9f5531f8c9
Alkosik <alkos.yt@gmail.com>
2021-05-06 12:57:49 +0200

    Update trackAdd.js

--- player/trackAdd.js
+++ player/trackAdd.js
@@ -1,3 +1,18 @@
+const {
+    MessageEmbed
+} = require('discord.js');
+
 module.exports = (client, message, queue, track) => {
-    message.channel.send(`${client.emotes.music} - **${track.title}** - dodano do kolejki.`);
+    var sembed = new MessageEmbed()
+    .setColor("GREEN")
+    .setTitle(`${client.emotes.music} Dodano do kolejki`)
+    .setDescription(`**${track.title}**`)
+    .setThumbnail(track.thumbnail)
+    .addFields(
+		{ name: 'Autor', value: track.author },
+		//{ name: 'Długość', value: track.lengthSeconds + 's', inline: true },
+		//{ name: 'Wyświetlenia', value: track.views, inline: true },
+	)
+    //message.channel.send(`${client.emotes.music} - **${track.title}** - dodano do kolejki.`);
+    message.channel.send(sembed);
 };
