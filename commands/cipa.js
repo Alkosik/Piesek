108559aa72989263826d21cda17b049bde72682f
Alkosik <alkos.yt@gmail.com>
2021-06-16 22:13:58 +0200

    Create cipa.js

---
+++ commands/cipa.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	category: 'Fun',
+	name: 'cipa',
+	description: 'Cipa',
+	callback: ({ message, args }) => {
+        const omegaKekw = message.guild.emojis.cache.find(emoji => emoji.name === 'OmegaKEKW');
+
+		(async () => {
+			const soltysEmbed = new Discord.MessageEmbed()
+                .setTitle('Ale cipa')
+				.setImage('https://i.imgur.com/qs17Hey.jpg')
+			message.channel.send(soltysEmbed);
+		})();
+	},
+};
